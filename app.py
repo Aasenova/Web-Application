9d8dd64743e6c82ce228d33d95cd66dfa8abc994
Aasenova <adriana.georgieva.asenova@gmail.com>
2023-11-08 13:58:07 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)  # name for the Flask app
+app = Flask(__name__) #Turn this file into Flask application
 
-
-@app.route('/')
+@app.route('/') #Here is the code which I want the server to execute, when user visit website
 def home():
     return render_template('home.html')
-
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
-    return f'{name} was a man who hated working. ' \
-           f'He decided to rob a bank and escape to a tropical island. ' \
-           f'He planned everything carefully and waited for the right moment. ' \
-           f'He entered the bank with a mask and a gun and demanded all the money. ' \
-           f'He stuffed the cash in his bag and ran out of the door. ' \
-           f'He jumped into his car and drove away as fast as he could. ' \
-           f'He was so happy that he started singing along to the radio. ' \
-           f'He did not notice the police sirens behind him. ' \
-           f'He also did not notice the sign that said “Road Closed”. ' \
-           f'He crashed into a pile of bricks and was arrested.'
+    return render_template('home.html', name=name)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/index', methods=['POST'])
+def index():
+    message = request.form['message']
+    return render_template('home.html', message=message)
